ff69dd061fdfa09fe71b6fe4bc1cbdf7aaa44518
Noic Noic <galanisd@csd.auth.gr>
2022-12-09 13:38:42 +0200

    db

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("test");
+import express from "express";
+import { db } from "./db.js";
+
+const app = express();
+app.use(express.json());
+
+app.listen(8800, () => {
+  console.log("connected");
+});
+
+app.get("/", (req, res) => {
+  res.json(" this is a res ");
+});
+
+app.get("/123", (req, res) => {
+  const q = "SELECT * FROM blog.users";
+  db.query(q, (error, data) => {
+    if (error) return res.json(error);
+    console.log("userid: ", data[0].id);
+    console.log("username: " + data[0].username);
+    return res.json(data);
+  });
+});
+
+app.get("/asdf", (req, res) => {
+  const q =
+    "INSERT INTO blog.USERS (`id`,`username`,`email`,`password`,`img` ) VALUES (?)";
+  const values = [
+    "2",
+    "second username",
+    "2nd email",
+    "2nd password",
+    "2nd img",
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return "error";
+    console.log(data);
+    return res.json(data);
+  });
+});

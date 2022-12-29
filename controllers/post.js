627df24bc33f7ecebb791be58d1974f93694420a
Noic Noic <galanisd@csd.auth.gr>
2022-12-29 21:28:51 +0200

    addPost from controller

--- controllers/post.js
+++ controllers/post.js
@@ -9,11 +9,29 @@ export const getPosts = (req, res) => {
 };
 
 export const getPost = (req, res) => {
-  res.json("from controller");
+  const q = "SELECT * FROM POSTS WHERE id = ?";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json(data[0]);
+  });
 };
 
 export const addPost = (req, res) => {
-  res.json("from controller");
+  const q =
+    "INSERT INTO blog.POSTS (`title`,`description`,`img`,`date`,`uid` ) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.img,
+    req.body.cat,
+    req.body.uid,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.json("Post has been created.");
+  });
 };
 
 export const deletePost = (req, res) => {

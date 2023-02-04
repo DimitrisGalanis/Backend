4812c2fa65cf925cb05ebc138fd1f88f04dfe748
Noic Noic <galanisd@csd.auth.gr>
2023-02-04 13:53:44 +0200

    changed limit of srv

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.use(express.json()); // app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: "25mb" }));
 app.use(cookieParser());
 app.use("/api/auth/", authRoutes);
 app.use("/api/posts/", postRoutes);
